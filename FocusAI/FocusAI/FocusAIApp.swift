e7cc5a70cf686b5d2a2b66687d6b2565874482d5
Charlie King <charlieking450@gmail.com>
2025-06-03 17:44:33 -0700

    Initial commit

---
+++ FocusAI/FocusAI/FocusAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusAIApp.swift
+//  FocusAI
+//
+//  Created by Charlie King on 6/3/25.
+//
+
+import SwiftUI
+
+@main
+struct FocusAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
