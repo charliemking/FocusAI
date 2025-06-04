06f131533d5e7dc7bd9d1377950af3f2b6ec9aeb
Charlie King <charlieking450@gmail.com>
2025-06-03 17:44:33 -0700

    Initial commit

---
+++ FocusAI/FocusAI/PDFInputView.swift
@@ -0,0 +1,18 @@
+//
+//  PDFInputView.swift
+//  FocusAI
+//
+//  Created by Charlie King on 6/3/25.
+//
+
+import SwiftUI
+
+struct PDFInputView: View {
+    var body: some View {
+        VStack {
+            Text("Upload a PDF")
+                .font(.title)
+                .padding()
+        }
+    }
+}
